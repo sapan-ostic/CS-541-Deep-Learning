a018fb15bcec99f74008a69690861e5e8623eac9
sapan-ostic <sapanagrawal777@gmail.com>
2020-02-15 14:29:33 -0500

    HW 4 Completed BackProp

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -1,140 +1,230 @@
 #!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
 
 import matplotlib.pyplot as plt
 import numpy as np
+import configparser
 
 # just to overwrite default colab style
 plt.style.use('default')
 plt.style.use('seaborn-talk')
 
+np.random.seed(10)
 
-# Linear Regression using Stochastic Gradient Descent
-def batchloader(X, Y, batchsize = 20):
+
+# In[2]:
+
+
+# Activations
+def relu(X):
+    return np.maximum(0,X)
+
+def softmax(X):
+    exp_x = np.exp(X)
+    probs = exp_x / np.sum(exp_x, axis=1, keepdims=True) # [N x K]
+    return probs
+
+def batchloader(X, Y, batchsize = 32):
     n = Y.shape[0]
     idx = np.random.choice(np.arange(n),size=batchsize,replace=False)
     X_batch = X[idx,:]
     Y_batch = Y[idx,:]
+    
     return X_batch, Y_batch
 
-def softmax(X):
-    exps = np.exp(X)
-    return exps / np.sum(exps)
-
-def crossEntropy(X, Y, theta):
-    fce = 0
-    
+def crossEntropy(X, Y, W):
     n, m = X.shape
     n, p = Y.shape 
+    fce = 0
+    network = forward(X, W)
+    Y_pred = network[-1]
+    Y_pred = np.asarray(Y_pred)
     
     for i in range(n):
-        Y_pred = softmax(np.dot(X[i],theta))
-        fce += np.dot(Y[i].T, np.log(Y_pred))  
+        fce += np.dot(Y[i].T, np.log(Y_pred[i]))  
     fce *= (-1/n)
     return fce
-    
-def SGD(X, Y, learning_rate=0.001, epochs=100, bs = 0.2, alpha = 0.2):
-    
-    n, m = X.shape
-    n, p = Y.shape
-    
-    w = np.random.randn(m,p)
-    b = np.random.randn(1,p)
-    
-    batchsize = round(bs*n)
-    
-    # preprocessing Data
-    X = np.append(X, np.ones((n,1)), axis=1)
-    
-    n, mpn = X.shape
 
-    COST = np.zeros(epochs)
-    theta = np.append(w, b, axis=0)
-    
-    for i in range(epochs):
-    
-        # Get Batch 
-        X_batch, Y_batch = batchloader(X, Y, batchsize) 
-        Y_pred = softmax(np.dot(X_batch,theta))
-        
-        # perform gradient descent
-        gradJ = (1/n)*( np.dot(X_batch.T, (Y_pred - Y_batch) )) 
-        theta = theta - learning_rate * gradJ - 2*alpha*np.append(w,np.zeros((1,p)),axis=0)
-         
-        w = theta[:m,:p]    
-
-        # get cost
-        COST[i] = crossEntropy(X_batch, Y_batch, theta)
-    
-    return theta, COST 
-    
 def accuracy(Y_pred, Y):
     n,p = Y.shape
     acc = np.sum([np.argmax(Y_pred[i])==np.argmax(Y[i]) for i in range(n)])/(0.01*n)
     return acc
 
+# Storing all weights ndarray as list
+def get_weights(layers):
+    weights = []
+    Nlayers = len(layers)
+    c = 10**(-0.5)
+    low = -c/2
+    high = c/2
+    
+    for i in range(Nlayers - 1):   
+        w = np.random.uniform(low=low, high=high, size=(layers[i],layers[i+1]))
+        w[-1] = 0.01   
+        weights.append(w)    
+    return weights
 
-# In[97]:
+def forward(X, W):
+    h = X  #h0 = X
+    network = [X]
+    
+    for w in W[:-1]:   # in => h1 => out
+        z = np.dot(h, w) # next layer
+        h = relu(z)      # ReLU activation
+        network.append(h)    
+    y_pred = softmax(np.dot(h, W[-1])) # output with softmax
+    network.append(y_pred)
+    
+    return network # h1, h2, ... y_hat
+
+def backprop(network, y, W, lr=0.01, alpha = 0.0001):
+    y_pred = network[-1]
+    dJ_dz = (y - y_pred) #*sigmoidPrime(y_pred) # Initial dJ_dz (deltas)
+    
+    n = len(network)
+    N = range(n)
+    dw = []
+    
+    for i in list(reversed(N))[:-1]:
+        dW = network[i-1].T.dot(dJ_dz) # (40, 10) , (785, 40)
+        dw.append(dW) # (785,40) (40,10) 
+        dJ_dz = dJ_dz.dot(W[i-1].T) # (128,10), (128,40)      
+        dJ_dz[network[i-1] <= 0] = 0
+        
+    dw = np.flip(np.asarray(dw))
+    for i in range(n-1):
+        Wreg = W[i]
+        Wreg[-1] = 0*Wreg[-1]
+        W[i] += lr*dw[i] + alpha*Wreg
+    return W 
+
+def SGD(X, Y, layers, learning_rate=0.1, epochs=100, bs = 32, alpha = 0.002 ):
+
+    n, m = X.shape
+    n, p = Y.shape
+    
+    # Initialize Weights
+    weights = get_weights(layers)
+
+    COST = np.zeros(epochs)
+    ACC = np.zeros(epochs)
+    
+    for i in range(epochs):
+        
+        # Get Batch 
+        X_batch, Y_batch = batchloader(X, Y, bs) 
+        
+        # Forward Pass
+        network = forward(X_batch,weights)
+
+        # Back Propagation
+        weights = backprop(network, Y_batch, weights, learning_rate, alpha)    
+
+        # get cost
+        COST[i] = crossEntropy(X_batch, Y_batch, weights)
+        Y_pred = forward(X_batch, weights)[-1]
+        acc = accuracy(Y_pred, Y_batch)
+        ACC[i] = acc  
+        
+    return weights, COST, ACC 
 
 
-a = np.array([[1,2,3,4], [4,5,6,7]])
-print([np.argmax(a[i])==3 for i in range(2)])
-print(a[1])
+# In[3]:
 
 
-# In[ ]:
+def findBestHyperparameters():
+    print('Tuning Hyperparameter . . .')
+    # Load data
+    Xtr = np.load("mnist_train_images.npy")
+    n,m = Xtr.shape
+    Xtr = Xtr.reshape((n,-1))
+
+    # preprocessing Data
+    Xtr = np.append(Xtr, np.ones((n,1)), axis=1)
+    n,m = Xtr.shape
+
+    Ytr = np.load("mnist_train_labels.npy")
+    p = Ytr.shape[1]
+
+    # Get Validation Set
+    Xv = np.load("mnist_validation_images.npy")
+    nv = Xv.shape[0]
+    Xv = Xv.reshape((nv,-1)) # feature vector is row vector 
+
+    # preprocessing on validation set 
+    Xv = np.append(Xv, np.ones((nv,1)), axis=1)
+
+    Yv = np.load("mnist_validation_labels.npy")
+
+    # Tune Hyper parameter
+    LR = [0.001, 0.01, 0.1, 0.5]
+    EPOCHS = [25, 50, 75, 100]
+    BATCHSIZE = [16, 32, 64, 128]
+    ALPHA = [0.000001, 0.000002, 0.00005, 0.00001]
+
+    # Define layers: Input Hidden Output
+    # Enter no. of perceptrons in each layer
+    layers = [m] + [40] + [p]
+
+    cost = 1000000
+    iter = 0
+
+    for lr_ in LR:
+        for epochs_ in EPOCHS:
+            for bs_ in BATCHSIZE:
+                for alpha_ in ALPHA:
+                    theta, COST, ACC = SGD(Xtr, Ytr, layers, learning_rate=lr_, epochs=epochs_, bs=bs_, alpha=alpha_) 
+                    loss = COST[-1]
+                    print("iter: ", iter, "accuracy: ", ACC[-1], ", loss: ", COST[-1])
+                    iter += 1 
+
+                    if(loss<cost):
+                        lr = lr_
+                        epochs = epochs_
+                        bs = bs_
+                        alpha = alpha_
+                        cost=loss
+
+    print('Hyperparameter tuning completed:')
+    print('learning rate: ', lr, ', Epochs: ', epochs, ', mini-batchsize: ', bs, ', alpha: ', alpha)
+    return layers, lr, epochs, bs, alpha
+
+
+# In[45]:
 
 
 # Load data
 Xtr = np.load("mnist_train_images.npy")
-n = Xtr.shape[0]
+n,m = Xtr.shape
 Xtr = Xtr.reshape((n,-1))
+
+# preprocessing Data
+Xtr = np.append(Xtr, np.ones((n,1)), axis=1)
+n,m = Xtr.shape
+
 Ytr = np.load("mnist_train_labels.npy")
+p = Ytr.shape[1]
 
 # Get Validation Set
 Xv = np.load("mnist_validation_images.npy")
 nv = Xv.shape[0]
 Xv = Xv.reshape((nv,-1)) # feature vector is row vector 
-Yv = np.load("mnist_validation_labels.npy")
 
 # preprocessing on validation set 
 Xv = np.append(Xv, np.ones((nv,1)), axis=1)
 
-# print(Xtr.shape, Ytr.shape, Xv.shape, Yv.shape)
+Yv = np.load("mnist_validation_labels.npy")
 
-# Tune Hyper parameter
-LR = [0.001, 0.005, 0.01, 0.05]
-EPOCHS = [50, 100, 200, 400]
-BATCHSIZE = [0.1, 0.2, 0.3, 0.5]
-ALPHA = [0.001, 0.002, 0.005, 0.01]
+# layers, lr, epochs, bs, alpha = findBestHyperparameters()
 
-cost = 1000000
-
-iter = 0
-
-for lr_ in LR:
-    for epochs_ in EPOCHS:
-        for bs_ in BATCHSIZE:
-            for alpha_ in ALPHA:
-                theta, COST = SGD(Xtr, Ytr, learning_rate=lr_, epochs=epochs_, bs=bs_, alpha=alpha_) 
-                
-                Y_pred = softmax(np.dot(Xv, theta))
-                accu = accuracy(Y_pred, Yv)
-                loss = COST[-1]
-                print("accracy: ", accu)
-                print("iter: ", iter, ", loss: ", COST[-1])
-                iter += 1 
-                
-                if(loss<cost):
-                    lr = lr_
-                    epochs = epochs_
-                    bs = bs_
-                    alpha = alpha_
-                    cost=loss
-
+print('Testing model . . .')
 # Training on tuned hyperparameters
-theta, COST = SGD(Xtr, Ytr, learning_rate=lr, epochs=epochs, bs=bs, alpha=alpha)
+theta, COST, ACC = SGD(Xtr, Ytr, layers, learning_rate=lr, epochs=epochs, bs=bs, alpha=alpha)
 
-plt.plot(COST)
+plt.plot(ACC)
 plt.xlabel('epochs')
 plt.ylabel('MSE')
 plt.title('Stochastic Gradient Descent with L2 Regularization')
@@ -147,13 +237,13 @@ X_te = X_te.reshape((n,-1))
 
 # preprocessing Data
 X_te = np.append(X_te, np.ones((n,1)), axis=1)
-
 yte = np.load("mnist_test_labels.npy")
-MSE_test = crossEntropy(X_te, yte, theta)
-Y_pred = softmax(np.dot(X_te, theta))
-accu = accuracy(Y_pred, yte)
-print('accuracy', accu)
-print('MSE on test data: ', MSE_test[0,0])
+
+CE = crossEntropy(X_te, yte, theta)
+
+print('Results: ')
+print('accuracy (mean over last 10 samples):', np.mean(ACC[-10:]))
+print('Cross entropy loss on test data: ', CE)
 print('Tuned Hyperparameters: ')
-print('learning rate: ', lr, ', Epochs: ', epochs, ', mini-batchsize (in %): ', bs*100, ', alpha: ', alpha)
+print('learning rate: ', lr, ', Epochs: ', epochs, ', mini-batchsize (in %): ', bs, ', alpha: ', alpha)
 
